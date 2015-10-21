d586893a755525e11df4c8e1410a27f441c98497
Clay Freeman <git@clayfreeman.com>
2015-10-21 14:17:31 -0500

    remove .DS_Store files

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+find . -type f -name ".DS_Store" -exec rm -f "{}" \;
 dpkg-deb -Zgzip -b deb
 VERSION=`dpkg-deb -f deb.deb Version`
 mv deb.deb com.clayfreeman.stash-cmds_${VERSION}.deb
